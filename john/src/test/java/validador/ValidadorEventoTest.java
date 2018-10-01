ea4ec5169834cb328e1730fc48c94d563a87afdf
felipe <felipe@ADMINRG-1PI46J6>
2018-09-30 21:29:12 -0300

    Ajuste nos nomes dos métodos de teste. Testes de evento concluídos.

--- john/src/test/java/validador/ValidadorEventoTest.java
+++ john/src/test/java/validador/ValidadorEventoTest.java
@@ -1,9 +1,19 @@
 package validador;
 
+import static org.junit.Assert.fail;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertEquals;
+
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Before;
 import org.junit.Test;
 
+
+
 import exception.CampoObrigatorioException;
+import exception.RegraNegocioException;
 import model.impl.Evento;
 import validador.impl.ValidadorEvento;
 
@@ -17,24 +27,67 @@ public class ValidadorEventoTest {
 	}
 	
 	@Test(expected = CampoObrigatorioException.class)
-	public void DeveGerarCampoObrigatorioExceptionQuandoNomeForNula() {
+	public void deveGerarCampoObrigatorioExceptionQuandoNomeForNula() {
 		Evento evento = new Evento();
 		
 		validador.validaNomeObrigatorio(evento);		
 	}
 	
 	@Test(expected = CampoObrigatorioException.class)
-	public void DeveGerarCampoObrigatorioExceptionQuandoNomeForBranco() {
+	public void deveGerarCampoObrigatorioExceptionQuandoNomeForBranco() {
 		Evento evento = new Evento("", null);
 		
 		validador.validaNomeObrigatorio(evento);		
 	}
 	
 	@Test(expected = CampoObrigatorioException.class)
-	public void DeveGerarCampoObrigatorioExceptionQuandoDataForNula() {
+	public void deveGerarCampoObrigatorioExceptionQuandoDataForNula() {
 		Evento evento = new Evento();
 		
 		validador.validaDataObrigatoria(evento);		
+	}
+	
+	public void deveValidarNomeSemGerarExecao() {
+		Evento evento = new Evento("Teste", null);
+		
+		validador.validaNomeObrigatorio(evento);
+	}
+	
+	public void deveInformarMensagemDaExeceptionCorretamentaParaNomeComCaracteresAlemDoMaximoPermitido() {
+		Evento evento = new Evento(RandomStringUtils.random(151), null);
+		
+		try{
+			validador.validaTamanhoMaximoNome(evento);
+			fail("Deve apresentar exception");
+		}catch (RegraNegocioException e) {
+			assertEquals("O campo Nome tem um tamanho de no máximo 150 caracteres", e.getMessage());
+		}
+	}
+	
+	public void deveGerarExcecaoQuandoDataDoEventoMenorQueDataAtual() {
+		LocalDate ontem = LocalDate.now().minusDays(1);
+		Evento evento = new Evento("teste", ontem);
+		
+		try {
+			validador.validaDataMaiorQueDataAtual(evento);
+			fail("Deve apresentar exception");
+		} catch (RegraNegocioException e) {
+			assertEquals("A data deve ser maior ou igual a data atual", e.getMessage());
+		}
+	}
+	
+	public void deveValidarDataAtualSemGerarExcecao() {
+		LocalDate hoje = LocalDate.now();
+		Evento evento = new Evento("teste", hoje);
+		
+		validador.validaDataMaiorQueDataAtual(evento);
+	}
+	
+	public void deveValidarDataFuturaSemGerarExcecao() {
+		LocalDate amanha = LocalDate.now().plusDays(1);
+		Evento evento = new Evento("teste", amanha);
+		
+		validador.validaDataMaiorQueDataAtual(evento);
 	}
 	
 }
