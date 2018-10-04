8dd644a910d7d03a7517ca4957a77c6ee89d10cd
felipe <felipe@PC_FELIPE>
2018-10-03 22:18:43 -0300

    Inclusão do período no vendaIngresso;

--- john/src/main/java/model/impl/VendaIngresso.java
+++ john/src/main/java/model/impl/VendaIngresso.java
@@ -1,7 +1,27 @@
 package model.impl;
 
+import model.impl.ov.Periodo;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import model.Ingresso;
+
 public class VendaIngresso {
 
+	private Periodo periodoDeVendaDeIngresso;
+	private Evento evento;
+	private List<Ingresso> ingressosDisponiveis;
+	
+	public VendaIngresso(Periodo periodoDeVenda) {
+		this.periodoDeVendaDeIngresso = periodoDeVenda;
+	}
+	
+	public void alteraDataFinalVendaIngresso(LocalDate novaDataFinal) {
+		Periodo novoPeriodo = new Periodo(this.periodoDeVendaDeIngresso.getDataInicial(), novaDataFinal);
+		this.periodoDeVendaDeIngresso = novoPeriodo;
+	}
+	
 	private static void inicioVendaDeIngresso() {
 
 	}
