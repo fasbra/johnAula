7c410643357f98b01c8e2fbdcc251adbe4177465
felipe <felipe@PC_FELIPE>
2018-10-03 23:37:28 -0300

    Criada estrutura de validação da venda de ingresso.

---
+++ john/src/main/java/validador/impl/ValidadorVendaIngresso.java
@@ -0,0 +1,22 @@
+package validador.impl;
+
+import model.impl.VendaIngresso;
+
+public class ValidadorVendaIngresso {
+
+	public void validaPeriodoObrigatorio(VendaIngresso vendaIngresso) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void validaEventoObrigatorio(VendaIngresso vendaIngresso) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void validaIngressoAdicionadoAVenda(VendaIngresso vendaIngresso) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- john/src/test/java/validador/ValidadorVendaIngressoTest.java
+++ john/src/test/java/validador/ValidadorVendaIngressoTest.java
@@ -1,5 +1,53 @@
 package validador;
 
-public class ValidadorVendaIngressoTest {
+import java.time.LocalDate;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import exception.CampoObrigatorioException;
+import model.impl.Evento;
+import model.impl.VendaIngresso;
+import validador.impl.ValidadorVendaIngresso;;
+
+public class ValidadorVendaIngressoTest {
+	
+	private ValidadorVendaIngresso validador;
+	
+	@Before
+	public void setUp() throws Exception {
+		validador = new ValidadorVendaIngresso();
+	}
+	
+	@Test(expected = CampoObrigatorioException.class)
+	public void deveGerarCampoObrigatorioExceptionQuandoPeriodoNulo() {
+		VendaIngresso vendaIngresso = new VendaIngresso(null);
+		validador.validaPeriodoObrigatorio(vendaIngresso);
+	}
+	
+	@Test(expected = CampoObrigatorioException.class)
+	public void deveGerarCampoObrigatorioExceptionQuandoEventoForNulo() {
+		VendaIngresso vendaIngresso = new VendaIngresso(null);
+		validador.validaEventoObrigatorio(vendaIngresso);
+	}
+	
+	@Test(expected = CampoObrigatorioException.class)
+	public void deveGerarCampoObrigatorioExceptionQuandoNaoExistirIngressosVinculadosAVenda() {
+		VendaIngresso vendaIngresso = new VendaIngresso(null);
+		validador.validaIngressoAdicionadoAVenda(vendaIngresso);
+	}
+	
+	@Test
+	public void deveInformarMensagemDaExceptionCorretamenteParaPeriodoInvalido() {
+	}
+	
+	@Test
+	public void deveValidarPeriodoSemGerarExecao() {
+	
+	}
+	
+	@Test
+	public void deveValidarIngressosDaVendaSemGerarExcecao() {
+		
+	}
 }
