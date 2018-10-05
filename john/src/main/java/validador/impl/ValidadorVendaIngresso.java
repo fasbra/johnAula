9ff24a9c846bfee95c0aa3b4c57443a35c9b663c
alexsandro.ferrao <alexsandro.ferrao@DBC-000360.mshome.net>
2018-10-04 21:04:09 -0300

    Criando validador para venda de Ingresso

--- john/src/main/java/validador/impl/ValidadorVendaIngresso.java
+++ john/src/main/java/validador/impl/ValidadorVendaIngresso.java
@@ -1,29 +1,45 @@
 package validador.impl;
 
-import org.apache.commons.lang.NotImplementedException;
+import java.time.LocalDate;
+import java.util.Date;
 
+import org.apache.commons.lang.NotImplementedException;
+import exception.CampoObrigatorioException;
+import exception.RegraNegocioException;
+import model.impl.Evento;
 import model.impl.VendaIngresso;
 
 public class ValidadorVendaIngresso {
 
+	private static final String PERIODO_OBRIGATORIO = "Período de venda obrigatório.";
+	private static final String EVENTO_OBRIGATORIO = "Evento obrigatório para venda de ingresso.";
+
+	private static final String DATA_FINAL_EVENTO_ANTERIOR_DATA_INICIAL = "A data final deve ser maior ou igual a data inicial do evento";
+
 	public void validaPeriodoObrigatorio(VendaIngresso vendaIngresso) {
-		// TODO Auto-generated method stub
-		throw new NotImplementedException();		
+		if (vendaIngresso.getPeriodoDeVendaDeIngresso() == null) {
+			throw new CampoObrigatorioException(PERIODO_OBRIGATORIO);
+		}
 	}
 
 	public void validaEventoObrigatorio(VendaIngresso vendaIngresso) {
-		// TODO Auto-generated method stub
-		throw new NotImplementedException();		
+		if (vendaIngresso.getEvento() == null) {
+			throw new CampoObrigatorioException(EVENTO_OBRIGATORIO);
+		}
 	}
 
 	public void validaIngressoAdicionadoAVenda(VendaIngresso vendaIngresso) {
 		// TODO Auto-generated method stub
-		throw new NotImplementedException();		
+		throw new NotImplementedException();
 	}
 
-	public void validaPeriodoComDataInicialMenorQueDataFinal(VendaIngresso vendaIngresso) {
-		// TODO Auto-generated method stub
-		throw new NotImplementedException();
+	public void validaPeriodoComDataInicialAnteriorADataFinal(VendaIngresso vendaIngresso) {
+		LocalDate dataInicioVenda = vendaIngresso.getPeriodoDeVendaDeIngresso().getDataInicial();
+		LocalDate dataFinalVenda = vendaIngresso.getPeriodoDeVendaDeIngresso().getDataFinal();
+
+		if (dataFinalVenda.isBefore(dataInicioVenda)) {
+			throw new RegraNegocioException(DATA_FINAL_EVENTO_ANTERIOR_DATA_INICIAL);
+		}
 	}
 
 	public void validaIngressosAVenda(VendaIngresso vendaIngresso) {
